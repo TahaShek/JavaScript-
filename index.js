f72fc325bfbbcf435b899012282989950a38a75f
TahaShek <sohailtaha128@gmail.com>
2022-09-01 02:38:09 +0500

    commit

--- index.js
+++ index.js
@@ -2,18 +2,86 @@ console.log('hello')
 
 
 
-let myfun2=()=>{
-    console.log('my func 2 is gere ')
-    console.log('name is this')
-}
+// let myfun2=()=>{
+//     console.log('my func 2 is gere ')
+//     console.log('name is this')
+// }
 
 
-let myfunc=(callback)=>{
-console.log('we here')
-    callback()
+// let myfunc=(callback)=>{
+// console.log('we here')
+//     callback()
 
 
 
-}
+// }
 
-myfunc(myfun2)
+// myfunc(myfun2)
+
+
+// Map method 
+
+// let number =[1,2,3,4,6];
+
+// let  result= number.map(element,index)=>{
+//     element
+// }
+// console.log(result)
+
+
+// let user =[
+//     {Name:'taha' ,age:21},
+//     {Name:'ali' ,age:23},
+//     {Name:'malik' ,age:34},
+//     {Name:'zain' ,age:443},
+// ]
+//    let username = user.map((user)=>{
+//     return user.Name
+//  })
+
+//  console.log(username)
+
+// let number=[1,2,3,4,5,6,7,8,9,7,8,,15,6];
+
+// let even=number.filter((element)=>{
+// //   return  element%2===0;
+// })
+
+// console.log(even)
+
+// console.log(even)()
+
+
+// let filter=(number,greater)=>{
+// return number.filter(element=>element>greater)
+// }
+
+// console.log(filter([1,2,3,4,5,6,7],6))
+
+// let number=[1,2,3,4,5,6]
+
+// let num=number.filter(num=> num>4)
+// console.log(num)
+
+
+// let actors=[
+//     {Name:'jonny',worth:1000},
+//     {Name:'jerry',worth:10},
+//     {Name:'kik',worth:200000},
+// ]
+// let result=actors.filter(actor=>actor.worth>50)
+//  let n=result.map(actor=>actor.Name).join(',')
+// console.log(n)
+
+
+
+// let sum=(number)=>{
+// let result=0;
+// for( let num of number){
+//     console.log(num)
+//     result+=num
+// }
+// return result;
+// }
+
+// console.log(sum([1,2,3,4,]))
