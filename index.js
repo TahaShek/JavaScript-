bf6f3fcd0d829ad1946c8638901ac0f7d0266e4b
TahaShek <sohailtaha128@gmail.com>
2022-09-01 01:47:42 +0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('hello')
+
+
+
+let myfun2=()=>{
+    console.log('my func 2 is gere ')
+    console.log('name is this')
+}
+
+
+let myfunc=(callback)=>{
+console.log('we here')
+    callback()
+
+
+
+}
+
+myfunc(myfun2)
